6c3f62bbf52f4dbbb2d1c1bb7f4037d63ba646a3
syamgot <syamgot@gmail.com>
2016-03-10 16:03:02 +0900

    first commit

---
+++ js/PhotoEditor.js
@@ -0,0 +1,312 @@
+/**
+ *
+ *
+ */
+
+
+var PhotoEditor = function(width, height, src){
+
+	this.editCtx = {
+		x 		: 0,
+		y 		: 0,
+		width  	: 0,
+		height 	: 0,
+		rotate 	: 0
+	}
+	this.defaultCtx = {}
+	this.editCtxIdx = 0;
+	this.editCtxLog = [];
+
+	this.canvas 	= this.makeCanvas(width, height);
+	this.canvasCtx 	= this.canvas.getContext('2d');
+	this.img 		= null;
+
+	this.maxX 		= 0;
+	this.maxY 		= 0;
+
+	this.log 		= false;
+
+	if (typeof src !== 'undefined') {
+		this.load(src);
+	}
+
+}
+
+
+PhotoEditor.CANVAS_ID = 'photo_editor_canvas';
+PhotoEditor.MIN_SCALE = 1;
+PhotoEditor.MAX_SCALE = 1.5;
+
+
+/**
+ *
+ * @param string src 
+ * @param function onload
+ * @param function onerror
+ */
+PhotoEditor.prototype.load = function(src, onload, onerror) {
+	var self = this;
+	self.img = new Image();
+	self.img.onerror = function() {
+		if ((typeof onerror).toLowerCase() === 'function') {
+			onerror();
+		}
+	}
+	self.img.onload = function(){
+		self.initEditCtx();
+		self.draw();
+		if ((typeof onload).toLowerCase() === 'function') {
+			onload();
+		}
+	};
+	self.img.src = src;
+}
+
+
+/**
+ *
+ */
+PhotoEditor.prototype.initEditCtx = function() {
+	this.editCtxIdx = 0;
+	this.editCtxLog = [];
+	var rect = this.editRect();
+	this.updateEditCtxPos(rect.x, rect.y, rect.w, rect.h);
+	this.defaultCtx = this.utils.copy(this.editCtx);
+}
+
+
+/**
+ * 編集矩形を作成する
+ *
+ */
+PhotoEditor.prototype.editRect = function() {
+
+	function make(priority) {
+		var sx, sy, sw, sh,
+			pos = {x:this.img.width/2,y:this.img.height/2};
+	
+		if (priority) {
+			sw = this.canvas.width * this.img.height / this.canvas.height;
+			sh = this.img.height;
+		}
+		else {
+			sw = this.img.width;
+			sh = this.canvas.height * this.img.width / this.canvas.width;
+		}
+
+		sx = pos.x - sw / 2;
+		sy = pos.y - sh / 2;
+	
+		return {x:sx, y:sy, w:sw, h:sh};
+	}
+
+	var priorityToHeight = this.canvas.height >= this.canvas.width;
+	var rect = make.call(this, priorityToHeight);
+
+	if (!priorityToHeight && rect.y < 0) {
+		rect = make.call(this, priorityToHeight);
+	}
+	else if (priorityToHeight && rect.x < 0) {
+		rect = make.call(this, !priorityToHeight);
+	}
+
+	return rect;
+
+}
+
+
+/**
+ * 座標と最大XY値を更新する
+ *
+ */
+PhotoEditor.prototype.updateEditCtxPos = function(x,y,width,height) {
+	this.editCtx.x 		= x;
+	this.editCtx.y 		= y;
+	this.editCtx.width 	= width;
+	this.editCtx.height = height;
+	this.maxX 			= this.editCtx.x * 2;
+	this.maxY 			= this.editCtx.y * 2;
+	this.updateEditCtxLog();
+}
+
+/**
+ * 編集ログを更新する
+ *
+ */
+PhotoEditor.prototype.updateEditCtxLog = function(x,y,w,h) {
+	if (this.log) {
+		this.editCtxLog.splice(this.editCtxIdx+1, this.editCtxLog.length-1);
+		this.editCtxLog.push(this.utils.copy(this.editCtx));
+		this.editCtxIdx = this.editCtxLog.length-1;
+	}
+}
+
+
+/**
+ *
+ */
+PhotoEditor.prototype.makeCanvas = function(width, height) {
+	return this.utils.makeElm('canvas', {
+		id 		: PhotoEditor.CANVAS_ID,
+		width 	: width,
+		height 	: height
+	});
+}
+
+
+/**
+ *
+ */
+PhotoEditor.prototype.draw = function() {
+
+	var sx 	= this.editCtx.x;
+	var sy 	= this.editCtx.y;
+	var sw 	= this.editCtx.width;
+	var sh 	= this.editCtx.height;
+	var deg = this.editCtx.rotate;
+
+	this.canvasCtx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+	this.canvasCtx.save();
+	this.canvasCtx.translate(this.canvas.width/2, this.canvas.height/2);
+	this.canvasCtx.rotate(deg * Math.PI / 180);
+    this.canvasCtx.translate( -1 * this.canvas.width/2, -1 * this.canvas.width/2 );    
+	this.canvasCtx.drawImage(this.img, sx, sy, sw, sh, 0, 0, this.canvas.width, this.canvas.height);
+	this.canvasCtx.restore();
+
+}
+
+
+/**
+ * 拡大縮小する
+ *
+ * 初期状態から指定の比率に変形させる
+ * MIN_SCALEからMAX_SCALEの範囲の値を受け付ける
+ *
+ * @param int scale 
+ */
+PhotoEditor.prototype.scale = function(scale) {
+
+	if (scale < PhotoEditor.MIN_SCALE || scale > PhotoEditor.MAX_SCALE) {
+		return;
+	}
+
+	scale = scale - 1;
+
+	var w = this.defaultCtx.width  	- this.defaultCtx.width * scale;
+	var h = this.defaultCtx.height 	- this.defaultCtx.height * scale;
+	var x = this.defaultCtx.x 	 	+ ( this.defaultCtx.width - w ) / 2;
+	var y = this.defaultCtx.y 	 	+ ( this.defaultCtx.width - h ) / 2;
+
+	this.updateEditCtxPos(x, y, w, h);
+
+	this.draw();
+
+}
+
+
+/**
+ * 回転させる
+ *
+ * 初期状態から指定の角度に変形させる
+ *
+ * @param int degree
+ */
+PhotoEditor.prototype.rotate = function(degree) {
+	this.editCtx.rotate = degree;
+	this.updateEditCtxLog();
+	this.draw();
+}
+
+
+/**
+ * 移動
+ *
+ * 引数の値を座標に加算する
+ *
+ * @param int x 
+ * @param int y
+ */
+PhotoEditor.prototype.move = function(x,y) {
+
+	var deg = this.editCtx.rotate % 360;
+
+	if (deg == 90 || deg == 270) {
+		var tmp = x; x = y; y = tmp;
+		if (deg == 90) {
+			x *= -1; y *= -1;
+		}
+		else if (deg == 270) {
+			x *= -1;
+		}
+	}
+
+	var moveX	= this.editCtx.x + x;
+	var moveY	= this.editCtx.y + y;
+
+	moveX = this.maxX < moveX ? this.maxX : 0 > moveX ? 0 : moveX;
+	moveY = this.maxY < moveY ? this.maxY : 0 > moveY ? 0 : moveY;
+
+	this.editCtx.x = moveX;
+	this.editCtx.y = moveY;
+
+	this.updateEditCtxLog();
+
+	this.draw();
+
+}
+
+
+/**
+ * 編集内容を初期状態に戻す
+ *
+ */
+PhotoEditor.prototype.clear = function() {
+	this.editCtx = $.extend({},this.defaultCtx);
+	this.updateEditCtxLog();
+	this.draw();
+}
+
+
+/**
+ *
+ */
+PhotoEditor.prototype.undo = function() {
+	if (this.editCtxIdx > 0) {
+		this.editCtxIdx--;
+		this.editCtx = this.utils.copy(this.editCtxLog[this.editCtxIdx]);
+		this.draw();
+	}
+}
+
+
+/**
+ *
+ */
+PhotoEditor.prototype.redo = function() {
+	if (this.editCtxIdx+1 < this.editCtxLog.length) {
+		this.editCtxIdx++;
+		this.editCtx = this.utils.copy(this.editCtxLog[this.editCtxIdx]);
+		this.draw();
+	}
+}
+
+
+/**
+ *
+ *
+ */
+PhotoEditor.prototype.utils = {
+	copy : function(obj) {
+		return JSON.parse(JSON.stringify(obj));
+	}, 
+	makeElm : function(name, props) {
+		var elm = document.createElement(name);
+		for (var name in props) {
+			elm[name] = props[name];
+		}
+		return elm;
+	}
+}
+
+
+
---
+++ js/main.js
@@ -0,0 +1,152 @@
+$(function(){
+
+	var editor = new PhotoEditor(320,320);
+	editor.load('data/image1.png', 
+		function(){
+			console.log('complete');
+		}, 
+		function(){
+			console.log('error!');
+		}
+	);
+
+
+
+
+
+
+
+
+	var degree = 0;
+	$('#rotate').click(function(){
+		degree += 90;
+		editor.rotate(degree);
+	});
+
+	var scale = 100;
+	$('#scale').change(function(){
+		var scale = $(this).val();
+		editor.scale(scale/100);
+	}).val(scale);
+
+	$('body').append($(editor.canvas));
+
+	var value = 10;
+	$('#up').click(function(){ editor.move(0,- value);});
+	$('#down').click(function(){ editor.move(0,value);});
+	$('#left').click(function(){ editor.move(- value,0);});
+	$('#right').click(function(){ editor.move(value,0);});
+
+	$('#clear').click(function(){ editor.clear();});
+	$('#undo').click(function(){ editor.undo();});
+	$('#redo').click(function(){ editor.redo();});
+
+
+	$('#IMG1').click(function(){ editor.load('data/image1.png'); $('#scale').val(100); });
+	$('#IMG2').click(function(){ editor.load('data/image2.png'); $('#scale').val(100); });
+	$('#IMG3').click(function(){ editor.load('data/image3.png'); $('#scale').val(100); });
+
+
+
+
+
+
+	(function(){
+		var isDragging = false, prevX, prevY;
+		function onMouseDown( event ) {
+			event.preventDefault();
+			isDragging = true;
+			prevX = event.clientX;
+			prevY = event.clientY;
+		}
+		function onMouseMove( event ) {
+			if ( isDragging === true ) {
+				var x = prevX - event.clientX;
+				var y = event.clientY - prevY;
+				prevX = event.clientX;
+				prevY = event.clientY;
+				editor.move(x,- y);
+			}
+		}
+		function onMouseUp( event ) {
+			isDragging = false;
+		}
+		editor.canvas.addEventListener( 'mousedown', onMouseDown, false );
+		editor.canvas.addEventListener( 'mousemove', onMouseMove, false);
+		editor.canvas.addEventListener( 'mouseup', onMouseUp, false );
+	})();
+
+
+	(function(){
+		function onMouseWheel( event ) {
+			event.preventDefault();
+			var delta = event.wheelDeltaY ?
+							event.wheelDeltaY : event.wheelDelta ? 
+								event.wheelDelta : event.detail ? 
+									event.detail : 0;
+			scale += delta > 0 ? 0.5 : -0.5;
+			scale = Math.min(Math.max(scale, 100), 150);
+			editor.scale(scale/100);
+		}
+		editor.canvas.addEventListener( 'mousewheel', onMouseWheel, false );
+		editor.canvas.addEventListener( 'MozMousePixelScroll', onMouseWheel, false);
+	})();
+
+
+	(function(){
+
+		// 
+		var isTouching = false, prevX, prevY;
+		var isGesturing = false, prevScale;
+
+		// touch event handlers
+		function onTouchStart( event ) {
+			event.preventDefault();
+			isTouching = true;
+			prevX = event.changedTouches[0].pageX;
+			prevY = event.changedTouches[0].pageY;
+		}
+		function onTouchMove ( event ) {
+			if ( isTouching && !isGesturing) {
+				var x = prevX - event.changedTouches[0].pageX;
+				var y = event.changedTouches[0].pageY - prevY;
+				prevX = event.changedTouches[0].pageX;
+				prevY = event.changedTouches[0].pageY;
+				editor.move(x,- y);
+			}
+		}
+		function onTouchEnd ( event ) {
+			isTouching = false;
+		}
+
+		// gesture event handlers
+		function onGestureStart( event ) {
+			event.preventDefault();
+			isGesturing = true;
+			prevScale = event.scale;
+		}
+		function onGestureChage( event ) {
+			if (isGesturing) {
+				scale += (event.scale - prevScale)*10;
+				prevScale = event.scale;
+				scale = Math.min(Math.max(scale, 100), 150);
+				console.log(scale);
+				editor.scale(scale/100);
+			}
+		}
+		function onGestureEnd( event ) {
+			isGesturing = false;
+		}
+
+		editor.canvas.addEventListener( 'touchstart', onTouchStart, false );
+		editor.canvas.addEventListener( 'touchmove', onTouchMove, false );
+		editor.canvas.addEventListener( 'touchend', onTouchEnd, false );
+		editor.canvas.addEventListener( 'gesturestart', onGestureStart, false );
+		editor.canvas.addEventListener( 'gesturechange', onGestureChage, false );
+		editor.canvas.addEventListener( 'gestureend', onGestureEnd, false );
+
+	})();
+
+
+});
+
