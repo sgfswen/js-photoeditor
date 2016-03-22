d485f81d7b0d8d6a773bc060ce3046f355d95b2c
syamgot <syamgot@gmail.com>
2016-03-22 16:49:50 +0900

    バグ修正

--- js/PhotoEditor.js
+++ js/PhotoEditor.js
@@ -188,11 +188,13 @@ PhotoEditor.prototype.drawBuffer = function() {
 
 	if (this.isReversed()) {
 		this.minX = (this.img.width  - this.img.height * scale)/2;
-		this.maxX = (this.img.width  - this.img.height * scale)/2 + this.defaultCtx.width  - this.editCtx.width;
+		this.maxX =  this.img.height < this.img.width
+				  ? (this.img.width  - this.img.height * scale)/2 + this.defaultCtx.height - this.editCtx.height
+				  : (this.img.height * scale - this.img.width )/2 + this.defaultCtx.width  - this.editCtx.width;
 		this.minY = (this.img.height - this.img.width  * scale)/2;
-		this.maxY = this.img.height > this.img.width 
+		this.maxY =  this.img.height > this.img.width 
 				  ? (this.img.height - this.img.width  * scale)/2 + this.defaultCtx.width  - this.editCtx.width
-				  : (this.img.width * scale - this.img.height)/2 + this.defaultCtx.height - this.editCtx.height;
+				  : (this.img.width  * scale - this.img.height)/2 + this.defaultCtx.height - this.editCtx.height;
 	}
 	else {
 		this.minX = 0;
